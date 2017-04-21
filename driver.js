028a3acebe22dd3b3a5bcad0d5b01c7a84244320
Kwame Robertson <kwame4@umbc.edu>
2017-04-20 23:42:29 -0400

    attempted to implement page flips. Commented out

--- driver.js
+++ driver.js
@@ -1,9 +1,39 @@
+// Called when page loads
+function init() {
+	// document.getElementById("choiceBox").style.display = 'none';
+	// document.getElementById("userInput").style.display = 'none';
+	// document.getElementById("trailInfoPageTwo").style.display = 'none';
 
 
-$(document).keypress(function(key)
-{
-  if(key.which == 13)
-  {
-    alert("selection was " + document.getElementById("userInput").value);
+	document.getElementById("userDirections").style.display = 'none';
+	document.getElementById("trailInfoPageOne").style.display = 'none';
+	document.getElementById("trailInfoPageTwo").style.display = 'none';
+}
+
+// test?
+$(document).keypress(function(key) {
+  if(key.which == 13) {
+  	var userInput = document.getElementById("userInput").value;
+    alert("selection was " + userInput);
+    document.getElementById("userInput").value = ""; // resets input
   } 
 });
+
+// How to call this function only when above userInput is 2?
+// function optionTwo() {
+// 	switchPage("choiceBox", "trailInfoPageOne");
+// 	switchPage("userInput", "userDirections");
+
+// 	$(document).keypress(function (e) {
+// 	  if (e.keyCode === 0 || e.keyCode === 32) {
+// 	    e.preventDefault()
+// 	    console.log('Space pressed')
+// 	    switchPage("trailInfoPageOne", "trailInfoPageTWo");
+// 	  }
+// 	});
+// // }
+
+// function switchPage(removeID, showID) {
+// 	document.getElementById(removeID).style.display = 'none';
+// 	document.getElementById(showID).style.display = 'block';
+// }
