c22f5942ff7bc9e38d874eeb9ad3f51efb3b51d8
Gottleib <peter.gottleib@comcast.net>
2017-05-14 23:32:25 -0400

    smoothed river animation, added food decrementation, randomized distance traveled

--- traveling.js
+++ traveling.js
@@ -2,12 +2,15 @@ game = JSON.parse(localStorage.getItem('currentGame'));
 var monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
+var distances = [ "50%", "40%", "30%","20%", "10%", "0%"];
+game.selectionset = 
 
 $(document).ready(function(){
   game.date = new Date(JSON.parse(localStorage.getItem('currentGame')).date);
   $("#date").html(monthNames[game.date.getMonth()]+" "+game.date.getDate()+", "+game.date.getFullYear());
   $("#next").html(game.nextDistance);
   $("#total").html(game.totalDistance);
+  $("#food").html(game.inventory.food);
 
 });
 travel();
@@ -20,16 +23,25 @@ async function travel()
     // > game.nextDistance, thus the remaining distance should be subtracted from 
     // nextDistance and added to totalDistance, otherwise miles() returns the proper value
     //alert("gonna switch");
+    movement = miles();
     await sleep(50);
-    animation();
+    animation(movement);
     await sleep(1500);
-    game.totalDistance += game.nextDistance - miles() < 0 ? game.nextDistance : miles();
-    game.nextDistance -= game.nextDistance - miles() < 0 ? game.nextDistance : miles();
+    console.log(game.nextDistance);
+    console.log(movement);
+    console.log(game.nextDistance - movement);
+    console.log(game.totalDistance + movement);
+    game.totalDistance += (game.nextDistance - movement < 0) ? game.nextDistance : movement;
+    game.nextDistance -= (game.nextDistance - movement < 0) ? game.nextDistance : movement;
     game.date.setDate(game.date.getDate()+1);
+    //console.log(game.party.length + " " + game.ration);
+    game.inventory.food = Math.max(0, game.inventory.food - game.ration*game.party.length);
     $("#date").html(monthNames[game.date.getMonth()]+" "+game.date.getDate()+", "+game.date.getFullYear()); 
     //.slice(0,5)+(game.date.slice(5,7)+1)+game.date.slice(7,);
     $("#next").html(game.nextDistance);
     $("#total").html(game.totalDistance);
+    $("#landmark").animate({left: distances[Math.floor(game.nextDistance/movement)]},500);
+    $("#food").html(game.inventory.food);
     //await sleep(750);
     //alert('done');
 
@@ -39,7 +51,7 @@ async function travel()
 function miles()
 {
   //hours traveled * mph (# of oxen) = pace*4 
-  return game.pace * 4 * Math.round(game.inventory.oxen*.2);
+  return Math.round(game.pace*4*game.inventory.oxen*.1*(Math.random()*(1.15 - .85)+.85));
 }
 
 
@@ -48,13 +60,11 @@ function sleep(ms)
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function animation()
+async function animation(movement)
 {
   //alert(game.nextDistance + "\n"+miles()+"\n"+(game.nextDistance-miles()));
-  for(i=0; i<Math.min(miles(), game.nextDistance); i++)
+  for(i=0; i<Math.min(movement, game.nextDistance); i++)
   {
-    $('#landmark').animate({left: 50+'%'});
-    await sleep(50);
     $('#travel').attr("src", "traveling2.png");
     await sleep(50);
     $('#travel').attr("src", "traveling1.png");
@@ -62,4 +72,5 @@ async function animation()
     $('#travel').attr("src", "traveling.png");
     await sleep(50);
   }
+
 }
