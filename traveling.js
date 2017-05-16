b0e91b4dc90190a215bb9c5f4958ee2bd9d60639
Gottleib <peter.gottleib@comcast.net>
2017-05-16 02:12:39 -0400

    added more functionality to traveling

--- traveling.js
+++ traveling.js
@@ -1,9 +1,8 @@
+// reloads the game
 game = JSON.parse(localStorage.getItem('currentGame'));
 var monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
+    "July", "August", "September", "October", "November", "December"];
 var distances = [ "50%", "40%", "30%","20%", "10%", "0%"];
-
 
 $(document).ready(function(){
   game.date = new Date(JSON.parse(localStorage.getItem('currentGame')).date);
@@ -11,6 +10,8 @@ $(document).ready(function(){
   $("#next").html(game.nextDistance);
   $("#total").html(game.totalDistance);
   $("#food").html(game.inventory.food);
+  $("#health").html(getHealthString(game));
+  $("#weather").html(game.weather);
 
 });
 travel();
@@ -27,10 +28,7 @@ async function travel()
     // nextDistance and added to totalDistance, otherwise miles() returns the proper value
     //chaos function
     // update health
-    for(i=0; i<game.party.length; i++)
-    {
-      game.party[i].health -= game.ration - game.pace - 
-    }
+    healthDegredation();
     movement = miles();
     await sleep(50);
     animation(movement);
@@ -50,6 +48,8 @@ async function travel()
     $("#total").html(game.totalDistance);
     $("#landmark").animate({left: distances[Math.floor(game.nextDistance/movement)]},500);
     $("#food").html(game.inventory.food);
+    $("#health").html(getHealthString());
+    $("weather").html(game.weather);
 
     //await sleep(750);
 
@@ -90,7 +90,55 @@ async function animation(movement)
 
 }
 
-function healthDegredation(currHealth)
+function healthDegredation()
 {
+  weatherEffect;
+  // is mild weather
+  if(game.weather == "cool" || game.weather == "warm")
+  {
+    weatherEffect = 1;
+  }
+  // is severe weather
+  else if(game.weather == "cold" || game.weather == "hot")
+  {
+    weatherEffect = 2;
+  }
+  // is snowy or rainy
+  else
+  {
+    weatherEffect = 3;
+  }
+  for(i=0; i<game.party.length; i++)
+  {
+    game.party[i].health -= 5*((4 - game.ration) - game.pace - game.party[i].illness - weatherEffect);
+  }
+}
 
+function getHealthString(game){
+  totalHealth = 0;
+  for(var i=0;i<5;i++){
+    totalHealth += game.party[i].health;
+  }
+  avgHealth = totalHealth/5;
+
+  if (avgHealth > FAIR_HEALTH){
+    return "Good"
+  } else if(avgHealth > POOR_HEALTH){
+    return "Fair"
+  } else if(avgHealth > VERY_POOR_HEALTH){
+    return "Poor"
+  } else {
+    return "Very Poor"
+  }
+}
+
+function chaos()
+{
+  // initial variable returned. if nothing happens to halt the flow of progress, return false
+  // else return true
+  stop = false;
+
+
+
+  return stop;
 }
